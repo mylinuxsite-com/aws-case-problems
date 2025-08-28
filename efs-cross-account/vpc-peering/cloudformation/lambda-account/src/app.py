673fe5be964def26cdc68ce238ece86ab0a6ea6c
Alfred P. Ricafort <alpotr@gmail.com>
2025-08-28 18:24:45 +0800

    Code clean-up

--- efs-cross-account/vpc-peering/cloudformation/lambda-account/src/app.py
+++ efs-cross-account/vpc-peering/cloudformation/lambda-account/src/app.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # You can reference EFS files by including your local mount path, and then
 # treat them like any other file. Local invokes may not work with this, however,
 # as the file/folders may not be present in the container.
-FILE = Path("/mnt/lambda/file")
+FILE = Path("/mnt/lambda/file.txt")
 
 def lambda_handler(event, context):
     wrote_file = False
